b0f48df093519983a33107b17b717eba50619fe9
Bilal EL CHAMI <bilal@uprizon.com>
2023-06-29 12:54:56 +0200

    Add Documentation Build Directory

---
+++ build/assets/js/30a24c52.0ade675e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation_api=self.webpackChunkdocumentation_api||[]).push([[453],{8605:a=>{a.exports=JSON.parse('{"label":"hello","permalink":"/blog/tags/hello","allTagsPath":"/blog/tags","count":2}')}}]);
---
+++ build/assets/js/e16015ca.900cd0af.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation_api=self.webpackChunkdocumentation_api||[]).push([[9700],{5688:a=>{a.exports=JSON.parse('{"label":"hola","permalink":"/blog/tags/hola","allTagsPath":"/blog/tags","count":1}')}}]);
