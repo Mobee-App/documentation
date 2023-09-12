18c9af8959eac9697d876fd235179c660e9f7059
Bilal EL CHAMI <bilal@uprizon.com>
2023-09-12 12:20:44 +0200

    Update project config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,9 +54,8 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Mobee-App/documentation/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
